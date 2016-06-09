fd20107766835de09bd195c19c0a889d2fd1189e
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-06-09 11:51:53 -0700

    udpate interface.js

--- script/interface.js
+++ script/interface.js
@@ -13,43 +13,58 @@ function toCode(code) {
 function getNewMethod(type) {
     switch (type) {
         case 'uint8_t':
+        case 'uint16_t':
             {
                 return 'json_object_new_int';
             }
         default:
-            return 'Unsupport Type';
+            {
+                let parts;
+                if (parts = type.match(/(\w+)_t/)) {
+                    return 'build' + S('-' + parts[1]).camelize().s;
+                }
+                return 'UnsupportType_' + type;
+            }
     }
 };
 
 function parseLine(line, context) {
     let parts = line.match(/typedef\s+struct\s+(\w+)\s+{/);
     if (parts) {
-        console.log(parts[1]);
         context.type = parts[1];
-        toCode('\r\n\r\nvoid build' + S('-' + context.type).camelize().s + '(struct json_object *params, struct ' + context.type + ' *' + context.type + ') {\r\n');
+        toCode('\r\nstruct json_object* build' + S('-' + context.type).camelize().s + '(struct ' + context.type + ' *' + context.type + ') {\r\n');
         toCode('    struct json_object *obj = json_object_new_object();\r\n');
     }
 
     parts = line.match(/\s+(\w+)\s+(\w+);/);
     if (parts) {
-        console.log(parts[1]);
-        console.log(parts[2]);
         let key = parts[1];
         let value = parts[2];
         toCode('    json_object_object_add(obj, "' + value + '", ' + getNewMethod(key) + '(' + context.type + '->' + value + '));\r\n');
     }
 
-    parts = line.match(/}\s+(\w+);/);
+    parts = line.match(/\s+(\w+)\s+\*(\w+);/);
     if (parts) {
-        console.log(parts[1]);
-        toCode('    json_object_object_add(params, "' + context.type + '", obj);\r\n');
+        let key = parts[1];
+        let value = parts[2];
+        toCode('    json_object_object_add(obj, "' + value + '", ' + getNewMethod(key) + '(' + context.type + '->' + value + '));\r\n');
+    }
+
+    parts = line.match(/.*?(\w+)\s+:\s+\d+[,;]/);
+    if (parts) {
+        let value = parts[1];
+        toCode('    json_object_object_add(obj, "' + value + '", json_object_new_int(' + context.type + '->' + value + '));\r\n');
+    }
+
+    parts = line.match(/}.*?(\w+);/);
+    if (parts) {
+        toCode('    return obj;\r\n');
         toCode('}\r\n');
     }
 };
 
 fs.readFile('./head.h', 'utf8', (err, data) => {
     if (err) throw err;
-    console.log(data);
     let lines = data.match(/[^\r\n]+/g);
 
     let context = {};
