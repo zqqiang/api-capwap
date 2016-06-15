5806fc86b00d2a35940c84083fe1429f847b8604
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-06-15 11:06:00 -0700

    update interface.js

--- script/interface.js
+++ script/interface.js
@@ -54,6 +54,54 @@ function parseLine(line, context) {
         context.indent = '    ';
     }
 
+    // match macAddr[];
+    parts = line.match(/\s+(\w+)\s+macAddr\[\];/);
+    if (parts) {
+        toSource(context.indent + 'json_object_object_add(obj, "macAddr", buildArray(' + context.type + '->macAddr, ' + context.type + '->macLen));\r\n');
+        context.last = 'attribute';
+    }
+
+    // match macAddr[CW_MAC_ADDR_LEN_MAX]
+    parts = line.match(/\s+(\w+)\s+macAddr\[CW_MAC_ADDR_LEN_MAX\];/);
+    if (parts) {
+        toSource(context.indent + 'json_object_object_add(obj, "macAddr", buildArray(' + context.type + '->macAddr, ' + context.type + '->macLen));\r\n');
+        context.last = 'attribute';        
+    }
+
+    // match macAddr[ETH_ALEN]
+    parts = line.match(/\s+(\w+)\s+macAddr\[ETH_ALEN\];/);
+    if (parts) {
+        toSource(context.indent + 'json_object_object_add(obj, "macAddr", buildArray(' + context.type + '->macAddr, ETH_ALEN));\r\n');
+        context.last = 'attribute';
+    }
+
+    // match key[CW_KEY_LEN_MAX]
+    parts = line.match(/\s+(\w+)\s+key\[CW_KEY_LEN_MAX\];/);
+    if (parts) {
+        toSource(context.indent + 'json_object_object_add(obj, "key", buildArray(' + context.type + '->key, CW_KEY_LEN_MAX));\r\n');
+        context.last = 'attribute';
+    }
+
+    // match key value[len]
+    parts = line.match(/\s+(\w+)\s+(\w+)\[(\d+)\];/);
+    if (parts) {
+        let key = parts[1];
+        let value = parts[2];
+        let len = parts[3];
+        if ('0' === len) {
+            if ('rates' === value) {
+                toSource(context.indent + 'json_object_object_add(obj, "' + value + '", buildArray(' + context.type + '->' + value + ', ' + context.type + '->rate_num));\r\n');
+            } else if ('ie' === value) {
+                toSource(context.indent + 'json_object_object_add(obj, "' + value + '", buildArray(' + context.type + '->' + value + ', ' + context.type + '->ie_len));\r\n');
+            } else if ('key' === value) {
+                toSource(context.indent + 'json_object_object_add(obj, "' + value + '", buildArray(' + context.type + '->' + value + ', ' + context.type + '->kLen));\r\n');
+            }
+        } else {
+            toSource(context.indent + 'json_object_object_add(obj, "' + value + '", buildArray(' + context.type + '->' + value + ', ' + len + '));\r\n');
+        }
+        context.last = 'attribute';
+    }
+
     // match type value
     parts = line.match(/\s+(\w+)\s+(\w+);/);
     if (parts) {
@@ -217,6 +265,21 @@ let topInterface = [
     'vsp_radio_sta_max',
     'vsp_max_distance',
     'vsp_vap_web_authserver',
+    'base_ac_desc',
+    'vsp_wtp_allow',
+    'vsp_wbh_sta',
+    'base_cw_timers',
+    'base_decrypt_err_rpt_period',
+    'vsp_sta_stats_interval',
+    'vsp_timers_interval',
+    'vsp_sta_cap_interval',
+    'vsp_wtp_led_dark',
+    'vsp_wtp_sta_max',
+    'base_vsp_telnet_enable',
+    'base_vsp_http_enable',
+    'base_vsp_https_enable',
+    'base_vsp_ssh_enable',
+    'vsp_mesh_eth_bridge',
 ];
 
 function addTopInterface() {
@@ -226,6 +289,17 @@ function addTopInterface() {
         toSource('    json_object_object_add(params, key, build' + S('-' + intf).camelize().s + '(' + intf + '));\r\n}\r\n');
         toHeader('void build' + S('-' + intf).camelize().s + 'Json(struct json_object *params, const char *key, struct ' + intf + ' *' + intf + ');\r\n');
     });
+};
+
+function addToolInterface() {
+    toSource('\r\nstruct json_object* buildArray(uint8_t *macAddr, uint8_t macLen) {\r\n');
+    toSource('    int i = 0;\r\n');
+    toSource('    struct json_object *array = json_object_new_array();\r\n');
+    toSource('    for(i = 0; i < macLen; ++i) {\r\n');
+    toSource('        json_object_array_add(array, json_object_new_int(macAddr[i]));\r\n');
+    toSource('    }\r\n');
+    toSource('    return array;\r\n');
+    toSource('}\r\n');
 };
 
 fs.readFile('./head.h', 'utf8', (err, data) => {
@@ -243,6 +317,8 @@ fs.readFile('./head.h', 'utf8', (err, data) => {
     });
 
     toSource('#include "cwJsonApi.h"\r\n');
+
+    addToolInterface();
 
     toHeader('#ifndef _CW_JSON_API_H\r\n');
     toHeader('#define _CW_JSON_API_H\r\n\r\n');
